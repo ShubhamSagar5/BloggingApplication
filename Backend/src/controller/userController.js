e13f9a2d12e302427410c3a26789902d71832ed3
shubham <shubhamsagar628@gmail.com>
2024-06-01 11:07:04 +0530

    Login Fumctionality Implement

--- Backend/src/controller/userController.js
+++ Backend/src/controller/userController.js
@@ -15,7 +15,7 @@ export const register = asyncHandler(async(req,res,next)=>{
     let user = await User.findOne({email})
 
     if(user){
-        return next (new ErrorHandler("User Already Register"))
+        return next (new ErrorHandler("User Already Register",400))
     }
 
     user = await User.create({
@@ -32,5 +32,36 @@ export const register = asyncHandler(async(req,res,next)=>{
  return res.status(200).json({
         success:true,
         message:"User Register Successfully"
+    })
+})
+
+
+export const login = asyncHandler(async(req,res,next)=>{
+
+    const {email,password,role} = req.body 
+
+    if(!email || !password || !role){
+        return next(new ErrorHandler("Please Provide All Deatils",400))
+    }
+
+    let user = await User.findOne({email})
+
+    if(!user){
+        return next(new ErrorHandler("User Not Found Please Provide valid Email"))
+    }
+
+    const checkPasswordIsValid = await user.isPasswordCorrect(password)
+
+    if(!checkPasswordIsValid){
+        return next (new ErrorHandler("Password Is Incorrect Please Provide Valid Password"))
+    }
+
+    if(role !== user.role){
+        return next(new ErrorHandler("User Role Not Match with Existing User"))
+    }
+
+    return res.status(200).json({
+        success:true,
+        message:"User login successfully"
     })
 })
