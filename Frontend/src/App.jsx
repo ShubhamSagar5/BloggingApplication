e597d1d81841a2c3b5da35345ac42c0a553defb6
shubham <shubhamsagar628@gmail.com>
2024-06-08 13:41:25 +0530

    Configure Routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './App.css'
+import {BrowserRouter as Router ,Routes,Route} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Register from './components/pages/Register'
+import Login from './components/pages/Login'
+import About from './components/pages/About'
+import AllAuthor from './components/pages/AllAuthor'
+import Blogs from './components/pages/Blogs'
+import Dashboard from './components/pages/Dashboard'
+import SingleBlog from './components/pages/SingleBlog'
+import UpdateBlog from './components/pages/UpdateBlog'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/authors' element={<AllAuthor/>}/>
+          <Route path='/blogs' element={<Blogs/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/blog/:id' element={<SingleBlog/>}/>
+          <Route path='/blog/update/:id' element={<UpdateBlog/>}/>
+        </Routes>
+      </Router>
+    </>
   )
 }
 
---
+++ Frontend/src/components/pages/AllAuthor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllAuthor = () => {
+  return (
+    <div>All</div>
+  )
+}
+
+export default AllAuthor
